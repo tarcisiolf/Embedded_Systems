a681a36a4e053e2b376387b06aec366a0512b103
Tarcísio <tlf@ic.ufal.br>
2019-10-29 10:18:58 -0300

    Revisão terminada

--- Revisao_C/Union.c
+++ Revisao_C/Union.c
@@ -18,6 +18,7 @@ struct packet_s {
     uint8_t c;
     uint16_t d;
 };
+
 struct packet_simple_s {
     uint8_t a;
     uint8_t b;
@@ -30,9 +31,10 @@ union int_array4 {
     uint8_t data[4];
 };
 
-
 union int_array4 x = {0};
 
+// Pode acessar a union byte_s através de content ou nibble ou por bit - obs: dentro da union
+// Guarda na memória um espaço de 8 bytes 
 union byte_s {
     uint8_t content;
     struct {
@@ -53,6 +55,7 @@ union byte_s {
 
 typedef union byte_s byte_t;
 
+// a:3 - reserva na memória um espaço para 3 bits
 struct packet_ninja_s {
     unsigned int a : 3;
     unsigned int b : 5;
@@ -69,13 +72,16 @@ struct packet_ninja_s {
 typedef struct packet_s packet_t;
 typedef struct packet_ninja_s packet_ninja_t;
 
-
+// packet_t: 8bytes->ab , 8bytes-> c, 16bytes->d
+// Função do tipo packet_t. Assim, devemos acessar a struct com '.' 
 packet_t packet_create(uint8_t a, uint8_t b, uint8_t c, uint16_t d)
 {
     packet_t pkt = {.ab = a | b << 3, .c = c, .d = d};
     return pkt;
 }
 
+// packet_ninja_t: 3bits->a, 5 bits->b; 8bytes->reserved, 8bytes->c, 16bytes->d; data[4]
+// packet passado como ponteiro. Assim, acessamos a struct usando '->'
 int packet_ninja_init(packet_ninja_t *pkt, uint8_t a, uint8_t b, uint8_t c, uint16_t d)
 {
     pkt->a = a;
@@ -85,16 +91,10 @@ int packet_ninja_init(packet_ninja_t *pkt, uint8_t a, uint8_t b, uint8_t c, uint
     return 0;
 }
 
+// Copia o conteúdo de data para pkt->data. Memcpy(destination, source, size)
+// Data + 1 pois o primeiro byte de data é reservado
 int packet_ninja_set(packet_ninja_t *pkt, uint8_t *data, size_t size)
 {
-    CHECK_CONDITION(size > 3);
-    CHECK_CONDITION(size > 3);
-    CHECK_CONDITION(size > 3);
-    CHECK_CONDITION(size > 3);
-    CHECK_CONDITION(size > 3);
-    CHECK_CONDITION(size > 3);
-    CHECK_CONDITION(size > 3);
-    CHECK_CONDITION(size > 3);
     CHECK_CONDITION(size > 3);
     // if (size > 3) {
     //     return -1;
@@ -104,6 +104,9 @@ int packet_ninja_set(packet_ninja_t *pkt, uint8_t *data, size_t size)
     // pkt->d = data[1] | data[2] << 8;
     return 0;
 }
+
+
+//pkt->d 16 bytes
 int packet_ninja_set_bytes(packet_ninja_t *pkt, uint8_t *data, size_t size)
 {
     if (size > 3) {
@@ -139,8 +142,7 @@ int main(int argc, char const *argv[])
            sizeof(struct packet_simple_s), sizeof(struct packet_ninja_s));
 
     byte_t x = {0xfe};
-    printf("Content %02Xh, nibble low: %Xh, high: %Xh", x.content, x.nibble.low,
-           x.nibble.high);
+    printf("Content %02Xh, nibble low: %Xh, high: %Xh\n", x.content, x.nibble.low, x.nibble.high);
     if (x.content & (1 << 2)) {
         x.content = x.content & ~(1 << 2);  // set the x.content[2] = 0;
         x.content = x.content | (1 << 2);   // set the x.content[2] = 1;
